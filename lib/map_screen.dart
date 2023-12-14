ba032205714fbcd4b2b0cd5c2cba9914fc9b51bb
humayunkabir11 <pythonloverkabir11@gmail.com>
2023-12-14 14:21:14 +0600

    feat(map_location): done

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,9 +1,8 @@
 
-
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapSample extends StatefulWidget {
   const MapSample({super.key});
@@ -16,37 +15,81 @@ class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller =
   Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+
+  Location location = new Location();
+
+  bool ?_serviceEnabled;
+  PermissionStatus ?_permissionGranted;
+  LocationData ?_locationData ;
+
+ getLocation()async{
+   _serviceEnabled = await location.serviceEnabled();
+   if (!_serviceEnabled!) {
+     _serviceEnabled = await location.requestService();
+     if (!_serviceEnabled!) {
+       return;
+     }
+   }
+
+   _permissionGranted = await location.hasPermission();
+   if (_permissionGranted == PermissionStatus.denied) {
+     _permissionGranted = await location.requestPermission();
+     if (_permissionGranted != PermissionStatus.granted) {
+       return;
+     }
+   }
+   _locationData = await location.getLocation();
+   setState(() {
+   });
+   print(_locationData);
+ }
+
+
+ @override
+  void initState() {
+    getLocation();
+
+    super.initState();
+  }
+  final Set<Marker> markers =  {};
+ addMarker (){
+   setState(() {
+     markers.add(
+        Marker(markerId: const MarkerId("current-location"),
+            position: LatLng(_locationData!.latitude!.toDouble(), _locationData!.longitude!.toDouble()),
+          icon: BitmapDescriptor.defaultMarker,
+          infoWindow: const InfoWindow(
+            title: "renti app"
+          )
+
+        ),
+
+     );
+   });
+ }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ),
-    );
-  }
+      body:_locationData!=null? SizedBox(
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+        child: GoogleMap(
+          // zoomControlsEnabled: false,
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target:LatLng(_locationData!.latitude!.toDouble(), _locationData!.longitude!.toDouble()),
+            // 23.764211402055338, 90.42693159752628
+            zoom: 11,
+          ),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+            addMarker();
+          },
+          markers: markers,
+        ),
+      ):const Center(child: CircularProgressIndicator(),),
+
+    );
   }
 }
